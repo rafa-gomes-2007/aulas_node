20660688a3a363c8458f07bd23ba324d4cf8da94
EA - Rafael Gomes Do Nascimento <nascimento_rafael@educadventista.org>
2023-09-25 15:13:48 -0300

    recapitulação de render HTML

--- 2_EXPRESS/5_recap/index.js
+++ 2_EXPRESS/5_recap/index.js
@@ -1,9 +1,13 @@
 const express = require("express") // importa o express
+const path = require("path") // path serve para transfomrar uma URL(./index.html por exemplo) relativa em uma URL absoluta
+
 
 const app = express()
 
+const caminho = path.join(__dirname, "templates")//dirname quer dizer pasta, esta linhna cria um camihno direto (URL) da pasta tamplates para o index.html
+
 app.get('/', (requisição, resposta) => {
-    resposta.send("Seja bem_vindo ao meu servidor!")
+    resposta.sendFile(`${caminho}/index.html`)
 }) // esta barra representa o loalhost 3000
 
 app.listen(3000 , () => {
